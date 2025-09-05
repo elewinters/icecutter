6f730a5c6c4b637147c2caf003cbc7bd48c646c2
ele <86350819+elewinters@users.noreply.github.com>
2025-09-05 16:24:44 +0200

    add checking for video length

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
-use std::process::{exit, Command, Stdio};
+use std::io;
 
-use iced::*;
+use std::error::Error;
+use std::process::{Command, Stdio};
 
 mod ui;
 
@@ -20,16 +21,43 @@ fn check_ffmpeg() -> bool {
     status.success()
 }
 
-fn main() {
+fn video_length(video: &str) -> Result<String, io::Error> {
+    let output = Command::new("ffprobe")
+        .arg("-i")
+        .arg(video)
+        .arg("-show_entries")
+        .arg("format=duration")
+        .arg("-v")
+        .arg("quiet")
+        .arg("-of")
+        .arg("csv=p=0")
+        .arg("-sexagesimal")
+        .output()?;
+
+    // get output from command and split by : and . so that we can get only the minutes and seconds
+    let output = String::from_utf8(output.stdout).unwrap();
+    let output: Vec<&str> = output.split(&[':', '.']).collect();
+
+    let minutes = &output[1];
+    let seconds = &output[2];
+
+    Ok(format!("{minutes}:{seconds}"))
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let file = args.get(1).cloned();
 
     if !check_ffmpeg() {
-        println!("failed to detect ffmpeg on this system");
-        exit(1);
+        return Err("failed to detect ffmpeg on this system".into());
     }
 
-    let window_settings = window::Settings {
+    let length = match file {
+        Some(ref file) => Some(video_length(file)?),
+        None => None,
+    };
+
+    let window_settings = iced::window::Settings {
         size: (640.0, 480.0).into(),
         resizable: false,
         ..Default::default()
@@ -41,10 +69,21 @@ fn main() {
             (
                 ui::State {
                     file: file.unwrap_or_default(),
+
+                    from: if length.is_some() {
+                        String::from("00:00")
+                    }
+                    else {
+                        Default::default()
+                    },
+
+                    to: length.unwrap_or_default(),
+                    
                     ..Default::default()
                 },
-                Task::none()
+                iced::Task::none()
             )
-        })
-        .unwrap();
+        })?;
+    
+    Ok(())
 }
