ea963dfd44bcc0970fda2c73b4e661f8e1c73060
ele <86350819+elewinters@users.noreply.github.com>
2025-09-05 15:28:54 +0200

    add ffmpeg check

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
 use std::env;
+use std::process::{exit, Command, Stdio};
+
 use iced::*;
 
 mod ui;
 
-fn main() -> iced::Result {
+// runs to check if ffmpeg is properly installed on the system
+fn check_ffmpeg() -> bool {
+    let command = Command::new("ffmpeg")
+        .arg("-version")
+        .stdout(Stdio::null()) // dont print ffmpeg output to console
+        .status();
+    
+    let status = match command {
+        Ok(status) => status,
+        Err(_) => return false,
+    };
+
+    status.success()
+}
+
+fn main() {
     let args: Vec<String> = env::args().collect();
     let file = args.get(1).cloned();
+
+    if !check_ffmpeg() {
+        println!("failed to detect ffmpeg on this system");
+        exit(1);
+    }
 
     let window_settings = window::Settings {
         size: (640.0, 480.0).into(),
@@ -24,4 +46,5 @@ fn main() -> iced::Result {
                 Task::none()
             )
         })
+        .unwrap();
 }
