31c6b5ea4ec4f81e67f6e822a38e544c5faf097e
ele <86350819+elewinters@users.noreply.github.com>
2025-11-25 15:36:15 +0100

    remove unnecessary async blocks when calling Task::perform

--- src/ui.rs
+++ src/ui.rs
@@ -199,12 +199,10 @@ impl State {
 
             // ran upon clicking the select button
             Message::SelectDialog => {
-                Task::perform(async {
+                Task::perform(
                     rfd::AsyncFileDialog::new()
                         .set_title("select video to convert")
-                        .pick_file()
-                        .await
-                    },
+                        .pick_file(),
                     Message::SelectDialogFinished // once the file dialog task is over, run this message
                 )
             }
@@ -255,14 +253,12 @@ impl State {
                     None => return Task::perform(error::show_error_async("invalid input file, failed to get file name from path"), |_| Message::None)
                 };
 
-                Task::perform(async move {
+                Task::perform(
                     rfd::AsyncFileDialog::new()
                         .set_title("save converted video")
                         .set_file_name(file_name)
                         .set_directory(directory.as_path())
-                        .save_file()
-                        .await
-                    },
+                        .save_file(),
                     Message::ConvertDialogFinished // once the file dialog task is over, run this message
                 )
             }
