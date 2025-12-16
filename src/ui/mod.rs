a139eedf2d8abe19c217decc10f7c7e30e62594a
ele <86350819+elewinters@users.noreply.github.com>
2025-12-16 22:28:59 +0100

    change version number to 1.4.1

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -164,7 +164,7 @@ impl State {
                 column![
                     // text column
                     column![
-                        text("icecutter v1.4.0")
+                        text("icecutter v1.4.1")
                             .size(30),
                         text("takes a video file, cuts it, and then compresses it down to 10MB or less with the specified configuration"),
                         text("primarly built for quickly cutting and compressing clips to upload to discord")
